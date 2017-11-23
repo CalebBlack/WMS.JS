e71b339eb61cce53fb157ee503b9b468971d244a
Caleb Black <caleb.paul.black@gmail.com>
2017-11-23 02:14:35 -0700

    update dependencies

--- src/components/background.js
+++ src/components/background.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import Router from './router';
-import Background from './components/background';
-import getCardDatabase from './redux/actions/getCardDatabase';
-import {initialize} from './redux/actions/login';
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
-import Header from './components/header';
-import './app.css';
-
-class App extends React.Component {
-  componentDidMount(){
-    this.props.dispatch(getCardDatabase());
-    this.props.dispatch(initialize());
-  }
-  render(){
-    return (
-      <div id='app'>
-        <Header/>
-        <Background/>
-        <Router/>
-      </div>
-    );
+import './background.css';
+class Background extends React.Component {
+  render() {
+    return (<img className='background' src='city.jpg'/>)
   }
 }
-export default withRouter(connect()(App));
+export default Background;

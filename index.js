57d001f0a175b8e303cdb21d73a551376919a6d0
Caleb Black <caleb.paul.black@gmail.com>
2017-11-23 02:19:07 -0700

    remove card game validation and update package.json to add server dependencies

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-try {
-  const cards = require('./resources/cards.json');
-} catch (error) {
-  throw new Error('Cannot Start, Missing/Invalid Card List');
-}
-
 const app = require('./app');
-const {sockets} = require('./sockets');
 const stringToInt = require('./functions/stringtoint');
 const fs = require('fs');
 const http = require('http');
@@ -24,14 +17,14 @@ if (!fs.existsSync('./build/source.js')) throw new Error('Source Not Built! Type
 
 if (port) {
   var httpServer = http.Server(app);
-  sockets(httpServer);
+  //sockets(httpServer);
   httpServer.listen(port);
   console.log(`Server Running on Port ${port}.`);
 } else {
   let lex = LEX.create({server:'staging',email:'sxuanchonline@gmail.com',agreeTos:true,approveDomains:['sxuan.ch','www.sxuan.ch']});
   let server = http.createServer(lex.middleware(require('redirect-https')()));
   let secureServer = https.createServer(lex.httpsOptions,lex.middleware(app))
-  sockets(secureServer);
+  //sockets(secureServer);
   console.log('Running Servers');
   server.listen(80);
   secureServer.listen(443)

c9c1d1f154a38c90949452a831fb2e1d4dbb4d54
Caleb Black <caleb.paul.black@gmail.com>
2017-12-02 16:01:59 -0700

    fix wrong user schema

--- schemas/user.js
+++ schemas/user.js
@@ -1,4 +1,9 @@
-const User = require('./user');
-const Session = require('./session');
-
-module.exports = {User,Session};
+module.exports = {
+  displayname: {type:String,required:true,unique:false,minlength:1},
+  username: {type:String,required:true,unique:true,lowercase:true,minlength:1},
+  email: {type:String,required:true,unique:true,lowercase:true},
+  created: { type: Date, default: Date.now },
+  hash: {type:String,required:true},
+  admin: {type:Boolean,required:true,default:false},
+  banned:{type:Boolean,required:true,default:false}
+}

2714daae29c05dbfbe275f31926c726a4298251b
Caleb Black <caleb.paul.black@gmail.com>
2017-12-02 15:36:52 -0700

    fix resource references

--- src/components/background.js
+++ src/components/background.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './background.css';
 class Background extends React.Component {
   render() {
-    return (<img className='background' src='background'/>)
+    return (<img className='background' src='/background'/>)
   }
 }
 export default Background;

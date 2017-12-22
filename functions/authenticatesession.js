f6bd08b01f20e81fe99dc8c22bde5756a18431c5
Caleb Black <caleb.paul.black@gmail.com>
2017-12-21 21:05:23 -0700

    add cookie auth to authenticatesession

--- functions/authenticatesession.js
+++ functions/authenticatesession.js
@@ -1,10 +1,11 @@
 const {Session} = require('../models');
 function authenticateSession(req){
   return new Promise((resolve,reject)=>{
-    if (req.headers && req.headers.session && typeof req.headers.session == 'string' && req.headers.session.length > 0) {
-      Session.findOne({_id:req.headers.session},(err,session)=>{
+    let session = req.headers && req.headers.session && typeof req.headers.session =='string' && req.headers.session.length > 0 ? req.headers.session : req.cookies && req.cookies.session && typeof req.cookies.session == 'string' && req.cookies.session.length > 0 ? req.cookies.session : null;
+    if (session) {
+      Session.findOne({_id:session},(err,session)=>{
         if (err) return reject(err);
-        if (!session) return reject(null);
+        if (!session) return reject('Session not found');
         resolve(session);
       });
     } else {
